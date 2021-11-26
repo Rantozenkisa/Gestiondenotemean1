5bf92ce62bf3f8e5fe217e5df70471cb4bc40566
Rantozenkisa <94462578+Rantozenkisa@users.noreply.github.com>
2021-11-26 13:23:26 +0100

    Gestion de note1

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { AccueilComponent } from './accueil/accueil.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    InscriptionComponent,
+    AccueilComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
